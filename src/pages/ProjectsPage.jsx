ca2aa0ad067e367e9cbb4f19952d601cba8feb63
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-04-04 13:09:30 +0200

    feat: wip

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,30 +2,30 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
 
 const ProjectsPage = () => {
   const [projects, setProjects] = useState([]);
 
+  //api 
+  // const apiLaravel = import.meta.env.VITE_API_URL;
+
+
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:8000/api/projects") // Endpoint API Laravel
-      .then((response) => {
-        setProjects(response.data);
+      .get("http://127.0.0.1:8000/api/projects") 
+      .then((resp) => {
+        setProjects(resp.data.data);
         
-        console.log(response.data);
+        console.log(resp.data);
       })
-      .catch((error) => {
-        console.error("Errore nel recupero dei progetti:", error);
-       
-      });
   }, []);
+    
+  
 
-//   if (loading) return <p>Caricamento in corso...</p>;
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Lista Progetti</h1>
+      <h1 className="text-center font-bold my-4">Lista Progetti</h1>
       <ul className="space-y-2">
         {projects.map((project) => (
           <li key={project.id} className="border p-4 rounded-lg shadow">
